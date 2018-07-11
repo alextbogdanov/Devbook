0c6cb6f24e4f6a3383c13aa32f56d8913d3f2570
Alexander Bogdanov <alex.bogdanovv14@gmail.com>
2018-07-11 14:02:35 +0300

    Added authorization to the backend

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb://alex:alex123@ds137089.mlab.com:37089/devbook'
+	mongoURI: 'mongodb://alex:alex123@ds137089.mlab.com:37089/devbook',
+	secretOrKey: 'secret'
 };
