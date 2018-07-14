f5f846ac1e6d6e2abceee5dee6db1029d5f07f56
Alexander Bogdanov <alex.bogdanovv14@gmail.com>
2018-07-15 00:18:41 +0300

    Added production keys for database

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-	mongoURI: 'mongodb://alex:alex123@ds137089.mlab.com:37089/devbook',
-	secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./keys_prod');
+} else {
+	module.exports = require('./keys_dev');
+}
